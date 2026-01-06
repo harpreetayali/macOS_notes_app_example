4bc9358d09152d0d6ca4a848aab125146eca229e
Harpreet Singh <singhsharpreet3@gmail.com>
2026-01-06 22:40:22 +0530

    Notes Mac App example

---
+++ NotesMacApp/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  NotesMacApp
+//
+//  Created by Harpreet Singh on 05/01/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
